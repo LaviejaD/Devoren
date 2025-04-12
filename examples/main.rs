3268855523d3f2a938ee622ddac2f2802ad7d05f
onlyd <dmmmgaymer@gmail.com>
2025-04-12 13:59:17 -0500

    update  example

--- examples/main.rs
+++ examples/main.rs
@@ -9,18 +9,20 @@ fn Index(request: Request) -> Response {
     }
     response
 }
-
-#[route(get,/test/<hol>)]
-fn Test(request: Request) -> Response {
+// http://127.0.0.1:8080/user/1
+#[route(get,/user/<id>)]
+fn Dynamic(request: Request) -> Response {
     let mut response = Response::default();
-    if let Ok(html) = fs::read_to_string("./public/index.html") {
-        response.body(html)
-    }
+    response.body(request.parameters.get("id").unwrap().to_owned());
+
     response
 }
 
 fn main() {
     let port = 8080;
     println!("http://127.0.0.1:{0}/", port);
-    Rex::new(port, 5).add_routes(Index).add_routes(Test).run();
+    Rex::new(port, 5)
+        .add_routes(Index)
+        .add_routes(Dynamic)
+        .run();
 }
